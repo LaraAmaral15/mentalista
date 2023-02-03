d858f3fc186274636b5bbaf166bd9fc9dd1e8aff
Lara <lara.lara@escola.pr.gov.br>
2023-02-02 21:05:18 -0300

    arruma os chutes

--- src/main.js
+++ src/main.js
@@ -20,30 +20,13 @@ function Chutar() {
     var resultado = document.getElementById("resultado")
     var chute = parseInt(document.getElementById("input-chute").value);
 
-    console.log(chute)
-
-     if (chute > 1000 || chute < 0 || isNaN(chute)) {
+    if (chute > 1000 || chute < 0 || isNaN(chute)) {
       imgCerebro.style.display= 'none';
       resultado.innerHTML = "Não...digite um número de 0 a 1000"
 
       lugarGif.innerHTML = `<img src="./src/img/nao.gif" alt="">`
       return
     } 
-
-    if(contador == 9) {
-      contador = 0;
-      imgCerebro.style.display= 'none';
-      tentativas.innerHTML ="Ah, as 10 tentativas acabaram! Será sorteado outro número";
-      resultado.innerHTML = "O número secreto era: " + numeroSecreto
-      lugarGif.innerHTML = `<img src="./src/img/sorry.gif">`
-      Sortear();
-
-      return
-    } else if (contador !== 10) {
-      contador++;
-      tentativas.innerHTML = "Tentativas: " + contador;
-    }
-
 
     if(chute == numeroSecreto) {
       imgCerebro.style.display= 'none';
@@ -62,6 +45,20 @@ function Chutar() {
       resultado.innerHTML = "Errou... o número secreto é maior que " + chute
 
       lugarGif.innerHTML = `<img src="./src/img/errou.gif">`
+    }
+
+    if(contador == 9) {
+      contador = 0;
+      imgCerebro.style.display= 'none';
+      tentativas.innerHTML ="Ah, as 10 tentativas acabaram! Será sorteado outro número";
+      resultado.innerHTML = "O número secreto era: " + numeroSecreto
+      lugarGif.innerHTML = `<img src="./src/img/sorry.gif">`
+      Sortear();
+
+      return
+    } else if (contador !== 10) {
+      contador++;
+      tentativas.innerHTML = "Tentativas: " + contador;
     }
   }
 }
